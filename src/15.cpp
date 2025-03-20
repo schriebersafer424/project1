0ca0397ee1078a507fbe0e74fdb62ac85b177c37
schriebersafer424 <schriebersafer424@gmail.com>
2025-03-20 21:44:08 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int result = a / b; // 运行结果为1.67
+
+    cout << "The division result is: " << result << endl;
+    return 0;
+}
