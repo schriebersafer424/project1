b02044d0172d975f4ea3a51b3cd638461a5db2a4
schriebersafer424 <schriebersafer424@gmail.com>
2025-04-11 11:39:52 +0000

    Add 29.cpp

---
+++ src/29.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 3;
+
+    if (x > y) {
+        std::cout << "x is greater than y." << std::endl;
+    } else {
+        std::cout << "y is greater than x." << std::endl;
+    }
+
+    return 0;
+}
