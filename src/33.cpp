4f24c358e07aac93b2f1e7df1ebca9ea2c4ac1dd
schriebersafer424 <schriebersafer424@gmail.com>
2025-04-27 01:46:57 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c;
+
+    // Add code here
+    cout << "a + b = " << a + b << endl;
+    cout << "c = a * b" << endl;
+
+    return 0;
+}
