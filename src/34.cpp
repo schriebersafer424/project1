3b5e261a34ae97feb71110d8ab35f3b7e204d408
schriebersafer424 <schriebersafer424@gmail.com>
2025-05-17 07:52:08 +0000

    Add 34.cpp

---
+++ src/34.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    if (x >= y) {
+        cout << "x is greater than or equal to y." << endl;
+    } else {
+        cout << "x is not greater than or equal to y." << endl;
+    }
+    return 0;
+}
